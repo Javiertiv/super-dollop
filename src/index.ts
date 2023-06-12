b1cb0dad28131374db9642dcf8bdbf0be1059a66
Javiertiv <135275224+Javiertiv@users.noreply.github.com>
2023-06-12 17:28:03 -0300

    add redCar and fix version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,27 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
 
-clampy.anchor.set(0.5);
+Loader.shared.add({url: "./pngwing.com.png", name: "redCar"});
+Loader.shared.add({url: "./clampy.png", name: "Clampy"});
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+Loader.shared.onComplete.add(()=>{
+	const clampy: Sprite = Sprite.from("Clampy");
+	const car: Sprite = Sprite.from("redCar")
 
-app.stage.addChild(clampy);
+	car.anchor.set(0);
+	car.x= app.screen.bottom/2;
+	car.y=app.screen.width/2;
+	car.x= 5;
+	car.y= 5;
+	app.stage.addChild(car); console.log("aca llega el auto")
+	
+	clampy.anchor.set(0);
+	clampy.x = app.screen.width / 8;
+	clampy.y = app.screen.height / 8;
+	clampy.x = 0;
+	clampy.y = 0;
+	app.stage.addChild(clampy); 
+	console.log("aca llega")
+});
+Loader.shared.load();
